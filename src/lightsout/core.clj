ba7c293c258ed993cbb9fa987b3afdcf95f6a9e6
mr-fotev <itsofaraona@abv.bg>
2014-11-19 03:22:02 +0200

    lights-out in Clojure

--- src/lightsout/core.clj
+++ src/lightsout/core.clj
@@ -7,15 +7,15 @@
 (defn move [v n]
   (letfn [(flip [y] (-> y v inc (rem 2)))]
     (cond 
-        (= n 0) (assoc v 0 (flip 0), 1 (flip 1), 5 (flip 5))
-	(= n 4) (assoc v 4 (flip 4), 3 (flip 3), 9 (flip 9))
-	(= n 20) (assoc v 20 (flip 20), 15 (flip 15), 21 (flip 21))
-	(= n 24) (assoc v 24 (flip 24), 23 (flip 23), 19 (flip 19)) 
-	(= (quot n 5) 0) (assoc v n (flip n), (dec n) (flip (dec n)), (inc n) (flip (inc n)), (+ n 5) (flip (+ n 5)))
-        (= (quot n 5) 4) (assoc v n (flip n), (dec n) (flip (dec n)), (inc n) (flip (inc n)), (- n 5) (flip (- n 5)))
-        (= (rem n 5) 0) (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (inc n) (flip (inc n)))
-        (= (rem n 5) 4) (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (dec n) (flip (dec n)))
-        :else (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (dec n) (flip (dec n)), (inc n) (flip (inc n))))))
+	  (= n 0) (assoc v 0 (flip 0), 1 (flip 1), 5 (flip 5))
+	  (= n 4) (assoc v 4 (flip 4), 3 (flip 3), 9 (flip 9))
+	  (= n 20) (assoc v 20 (flip 20), 15 (flip 15), 21 (flip 21))
+	  (= n 24) (assoc v 24 (flip 24), 23 (flip 23), 19 (flip 19)) 
+	  (= (quot n 5) 0) (assoc v n (flip n), (dec n) (flip (dec n)), (inc n) (flip (inc n)), (+ n 5) (flip (+ n 5)))
+      (= (quot n 5) 4) (assoc v n (flip n), (dec n) (flip (dec n)), (inc n) (flip (inc n)), (- n 5) (flip (- n 5)))
+      (= (rem n 5) 0) (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (inc n) (flip (inc n)))
+      (= (rem n 5) 4) (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (dec n) (flip (dec n)))
+      :else (assoc v n (flip n), (- n 5) (flip (- n 5)), (+ n 5) (flip (+ n 5)), (dec n) (flip (dec n)), (inc n) (flip (inc n))))))
 	   
 (def x (atom (solvable-board)))
 
@@ -24,21 +24,21 @@
 (defn init []
   (xyz-panel :items 
     (conj
-	(vec (for [i (range 5), j (range 5), :let [d (+ (* 5 i) j)]]
-		(button :background (if (= (@x d) 0) (color 250 250 50) (color 100 100 255))
-			:bounds [(+ 45 (* j 61)) (+ 35 (* i 61)) 60 60]
-	 		:listen [:action (fn [e] (do (swap! x move d) 
-		 		                     (config! game-window :content (init))
-                                                     (when (= (vec (repeat 25 0)) @x) 
-							(alert "You win!") 
-							(swap! x (fn [y] (solvable-board)))
-                                                        (config! game-window :content (init)))))])))
-	
-	  	(button :text "New game" :foreground :white :font "ARIAL-BOLD-18"
-			:background :blue
-			:bounds [400 100 150 60]
-			:listen [:action (fn [e] (do (swap! x (fn [y] (solvable-board))) (config! game-window :content (init))))]))))
-
+	  (vec (for [i (range 5), j (range 5), :let [d (+ (* 5 i) j)]]
+		     (button :background (if (= (@x d) 0) (color 250 250 50) (color 100 100 255))
+					 :bounds [(+ 45 (* j 61)) (+ 35 (* i 61)) 60 60]
+	 			     :listen [:action (fn [e] (do (swap! x move d) 
+		 		                                  (config! game-window :content (init))
+                                                  (when (= (vec (repeat 25 0)) @x) 
+										  		    (alert "You win!") 
+									 		        (swap! x (fn [y] (solvable-board)))
+                                                    (config! game-window :content (init)))))])))
+													
+	  (button :text "New game" :foreground :white :font "ARIAL-BOLD-18"
+			  :background :blue
+			  :bounds [400 100 150 60]
+			  :listen [:action (fn [e] (do (swap! x (fn [y] (solvable-board))) (config! game-window :content (init))))]))))
+															
 (config! game-window :content (init))
 
 (defn -main [& args] (show! game-window))
